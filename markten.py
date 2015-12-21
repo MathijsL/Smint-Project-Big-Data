f0f01178d57dcbe490f6f332d420f31297a402bc
saragedik <sara@gedik.org>
2015-12-21 01:25:00 +0100

    nieuwe versie markten.py
    het is nog niet af

--- markten.py
+++ markten.py
@@ -1,15 +1,32 @@
-import csv
-with open('markten.csv', 'rb') as f:
-    regel = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
-    for row in regel:
-        print row[0], row [1], row[2]
 
-#vragen aan Mathijs:
-#(1) op welke manier wil je de marktgegevens uitvragen? krijgen we een parameter door,
-# bijv 0 voor alles, 1 voor maandag, 2 voor dinsdag etc en wat geven we dan terug? een tabel? een bestandje?
+def markten(week):
+    import csv
+    with open('markten.csv', 'rb') as f:
+        regel = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+        # zorg er eerst voor dat de csv in een list wordt gelezen
+        csvinfo = []
+        csvinfo = [ [ None for i in range(3) ] for j in range(20) ]
+        print len(csvinfo)
+        x = 0
+        for row in regel:
+            rij=(row[0], row [1], row[2])
+            #print rij
+            csvinfo[x]=(rij);
+            x += 1
 
-#(2) bovenstaande moet waarsch in een functie. Geldt dat ook voor de import-functie? hoe zorgen we ervoor dat
-#het csv maar 1 wordt ingelezen en niet bij elke verandering van een vinkje. 
+    print
+    print csvinfo[0]
+    print csvinfo
 
 
+    #daarna info uit tuple in een list of tuple zetten om deze als return waarde terug te geven
+    for x in range(0,len(week)):
+        if week[x] == 1:
+            print csvinfo[x]
+        else:
+            print "indexnummer is: " , x , " hier dus niets printen of appenden"
 
+#week is een tuple die 7 dagen bevat, begint bij maandag: indien nul dan staat het vinkje kennelijk uit, anders aan.
+week = (0,1,0,0,0,0,0)      #hiermee wordt dus alleen dinsdag opgevraagd
+print week
+markten(week)
