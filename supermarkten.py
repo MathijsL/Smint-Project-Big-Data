0377a4b498fde4c83586e7dd94d22051b2993ab6
saragedik <sara@gedik.org>
2016-01-11 15:40:11 +0100

    supermarkten.py
    let op: supermarkten.csv is nog niet de versie met de aangepaste lat en
    long

---
+++ supermarkten.py
@@ -0,0 +1,26 @@
+## supermarkten.py
+## geeft 5 kolommen in deze volgorde terug:
+## (0) naam vd supermarkt,
+## (1) straat incl huisnr,
+## (2) poko,
+## (3) latitude,
+## (4) longitude
+
+
+def supermarkten():  #ontvangt geen parameter
+    import csv
+    with open('supermarkten.csv', 'rb') as f:
+        regel = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+        csvinfo = []
+        csvinfo = [ [ None for i in range(5) ] for j in range(200) ]
+        #print len(csvinfo)
+        x = 0
+        for row in regel:
+            rij=(row[0], row [1], row[2], row[3], row[4])
+            #print rij
+            csvinfo[x]=(rij);
+            x += 1
+        return csvinfo
+
+tabsuper = supermarkten()
+#print tabsuper
